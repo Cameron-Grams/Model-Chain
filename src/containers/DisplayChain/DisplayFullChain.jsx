aceec52c85a0b835fffb91829f27a64da2102510
Cameron <cameron.grams.2@gmail.com>
2018-05-15 15:56:13 -0400

    update

--- src/containers/DisplayChain/DisplayFullChain.jsx
+++ src/containers/DisplayChain/DisplayFullChain.jsx
@@ -3,6 +3,11 @@ import { connect } from 'react-redux';
 import LedgerBlock from './LedgerBlock'; 
 
 // update the type of blocks to reflext the dynamic content....
+// read in the field array from the array 
+// https://redux-form.com/7.3.0/examples/fieldarrays/
+// https://medium.com/@franleplant/react-higher-order-components-in-depth-cf9032ee6c3e
+// https://hackernoon.com/higher-order-components-hocs-for-beginners-25cdcf1f1713
+
 
 const DisplayFullChain = ( props ) => {
 
