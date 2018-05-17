29f24f05386cd456d13ba2f8b8b592f4e165b831
cameron <cameron.grams.2@gmail.com>
2018-05-17 00:21:43 -0400

    work the dynamic form

--- src/containers/DisplayChain/DisplayFullChain.jsx
+++ src/containers/DisplayChain/DisplayFullChain.jsx
@@ -2,26 +2,6 @@ import React from 'react';
 import { Field, FieldArray, reduxForm } from 'redux-form'
 import LedgerBlock from './LedgerBlock'; 
 
-const renderField = ({ input, label, type, meta: { touched, error } }) => (
-  <div>
-    <label>{label}</label>
-    <div>
-      <input {...input} type={type} placeholder={label} />
-      {touched && error && <span>{error}</span>}
-    </div>
-  </div>
-)
-
-// this can probably be developed in the LedgerBlock component....
-const renderBlocks = ( ... ) => {
-    <h3>Block Title</h3>
-    <h4>Block Number</h4>
-    <input>Block Data</input>
-    <p>Block Hash</p>
-    <p>Block Signature</p>
-};
-
-
 const DisplayFullChain = ( props ) => {
     return(
         <FieldArray name="dynamicChain" component={ renderBlocks } /> 
@@ -39,12 +19,3 @@ DisplayFullChain = reduxForm({
 
 export default connect( mapStateToProps, {} )( DisplayFullChain ); 
 
-
-// render forms for data and 
-//  - dynamic calculation of hash that will lead to changes in class
-//        - these changes in class will depend on the comparision of the current values ( in that block and the others derived from it )
-//          with the value for the given blocks held in state in the Redux store
-//  > can we do the same for the signature? 
-// static values for:
-//   - block title 
-//   - block number
--- src/containers/Ledger/LedgerBlock/LedgerBlock.jsx
+++ src/containers/Ledger/LedgerBlock/LedgerBlock.jsx
@@ -1,19 +1,60 @@
 import React from 'react';
-import { connect } from 'react-redux'; 
-import Carriage from '../../components/Carriage/Carriage'; 
+import { Field, FieldArray, reduxForm } from 'redux-form'
+import Carriage from '../../../components/Carriage/Carriage'; 
 
+const renderField = ({ input, label, type, meta: { touched, error } }) => (
+  <div>
+    <label>{label}</label>
+    <div>
+      <input {...input} type={type} placeholder={label} />
+      {touched && error && <span>{error}</span>}
+    </div>
+  </div>
+)
 
-const LedgerBlock = ( props ) => {
+const renderData = ( { fields, meta: { error } }) => (
+    <div>
+    {fields.map(( data, index ) => (
+      <div key={ index }>
+        <Field
+          name={ data }
+          type="text"
+          component={ renderField }
+          label={`Block: #${index + 1}`}
+        />
+      </div>
+    ))}
+    {error && <li className="error">{error}</li>}
+    </div>
+)
 
-    return(
+const LedgerBlock = ( { fields, meta: { error, submitFailed } } ) => (
+    <div>
+    { fields.map( ( block, index ) => (    
         <Carriage >
-            <h2>Block Title: { props.block.blockTitle }</h2>
-            <h4>Block Data: { props.block.blockData }</h4>
-            <p>Block Hash: { props.block.blockHash }</p>
-            <p>Block Signature: { props.block.blockSignature }</p>
-            <p>Key: { props.blockIdentifier }</p>
+            <h2>Block Title: { block.blockTitle }</h2>
+            <h4>Block Data: { block.blockData }</h4>
+            <p>Block Hash: { block.blockHash }</p>
+            <p>Block Signature: { block.blockSignature }</p>
+            <p>Key: { block.blockIdentifier }</p>
+            
+        <FieldArray name={`${ block }.blockData`} component={ renderData } />
         </Carriage>
-    )
-}
+    ) ) }
+    </div> )
 
-export default LedgerBlock;
+
+export default reduxForm( {
+  form: 'blockArrays', 
+})( LedgerBlock ); 
+
+
+
+// render forms for data and 
+//  - dynamic calculation of hash that will lead to changes in class
+//        - these changes in class will depend on the comparision of the current values ( in that block and the others derived from it )
+//          with the value for the given blocks held in state in the Redux store
+//  > can we do the same for the signature? 
+// static values for:
+//   - block title 
+//   - block number
